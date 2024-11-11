c6550074593d0cf3f32889ff8d17a406644af49b
aster <137767097+aster-void@users.noreply.github.com>
2024-11-11 12:47:12 +0900

    biome: use spaces for indenting

--- main.ts
+++ main.ts
@@ -11,70 +11,70 @@ const NOTION_TASK_PAGE_URL = "https://www.notion.so/utcode/e8d7215fb5224be4a9a3e
 const DAY = 24 * 60 * 60 * 1000;
 
 const query = JSON.stringify({
-	filter: {
-		and: [
-			{
-				property: "期日",
-				date: {
-					before: new Date(Date.now() + 3 * DAY).toISOString().match(/^\d{4}-\d{2}-\d{2}/)?.[0],
-				},
-			},
-			{
-				property: "対応済",
-				checkbox: {
-					equals: false,
-				},
-			},
-		],
-	},
-	sorts: [
-		{
-			property: "期日",
-			direction: "ascending",
-		},
-	],
+  filter: {
+    and: [
+      {
+        property: "期日",
+        date: {
+          before: new Date(Date.now() + 3 * DAY).toISOString().match(/^\d{4}-\d{2}-\d{2}/)?.[0],
+        },
+      },
+      {
+        property: "対応済",
+        checkbox: {
+          equals: false,
+        },
+      },
+    ],
+  },
+  sorts: [
+    {
+      property: "期日",
+      direction: "ascending",
+    },
+  ],
 });
 
 const NotionFetchResponse = v.object({
-	results: v.array(
-		v.object({
-			properties: v.object({
-				期日: NotionTypes.date,
-				タイトル: NotionTypes.title,
-				担当者: v.array(NotionTypes.user),
-			}),
-		}),
-	),
+  results: v.array(
+    v.object({
+      properties: v.object({
+        期日: NotionTypes.date,
+        タイトル: NotionTypes.title,
+        担当者: v.array(NotionTypes.user),
+      }),
+    }),
+  ),
 });
 
 async function main() {
-	const tc = new TypeChecker();
+  const tc = new TypeChecker();
 
-	const response = await fetch("https://api.notion.com/v1/databases/e8d7215f-b522-4be4-a9a3-e7d3be4d41ff/query", {
-		method: "POST",
-		headers: {
-			"Notion-Version": "2022-06-28",
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
-		},
-		body: query,
-	});
+  const response = await fetch("https://api.notion.com/v1/databases/e8d7215f-b522-4be4-a9a3-e7d3be4d41ff/query", {
+    method: "POST",
+    headers: {
+      "Notion-Version": "2022-06-28",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
+    },
+    body: query,
+  });
 
-	const json = tc.check("notion fetch response", await response.json(), NotionFetchResponse);
+  const json = tc.check("notion fetch response", await response.json(), NotionFetchResponse);
 
-	const tasks = json.results
-		.map((result) => {
-			const due: string = result.properties.期日.date.start;
-			const title: string = result.properties.タイトル.title.map((title) => title.plain_text).join("");
-			const assignee: string = result.properties.担当者.map((u) => u.name).join(", ");
+  const tasks = json.results
+    .map((result) => {
+      const due: string = result.properties.期日.date.start;
+      const title: string = result.properties.タイトル.title.map((title) => title.plain_text).join("");
+      const assignee: string = result.properties.担当者.map((u) => u.name).join(", ");
 
-			return `・【${due}】${title} (${assignee})`;
-		})
-		.join("\n");
-	let message =
-		json.results.length === 0
-			? "本日は期限が迫っているタスクはありませんでした。"
-			: `
+      return `・【${due}】${title} (${assignee})`;
+    })
+    .join("\n");
+  let message =
+    json.results.length === 0
+      ? "本日は期限が迫っているタスクはありませんでした。"
+      : `
 3日以内に期限が迫っているタスクがあります！
 ${tasks}
 
@@ -82,27 +82,27 @@ ${tasks}
 > <${NOTION_TASK_PAGE_URL}|運営タスク>
 `.trim();
 
-	if (tc.hasFailed()) message += `---\n 一つ以上の型チェックが失敗しました: ${tc.errors}`;
+  if (tc.hasFailed()) message += `---\n 一つ以上の型チェックが失敗しました: ${tc.errors}`;
 
-	// not appending this to messages, as it includes secrets
-	const { err, val: webhook } = check("env SLACK_WEBHOOK_URL", process.env.SLACK_WEBHOOK_URL, Url);
-	if (err) {
-		console.error(`Failed to parse webhook. first and last characters are as follows.
+  // not appending this to messages, as it includes secrets
+  const { err, val: webhook } = check("env SLACK_WEBHOOK_URL", process.env.SLACK_WEBHOOK_URL, Url);
+  if (err) {
+    console.error(`Failed to parse webhook. first and last characters are as follows.
 		first: ${webhook.at(0)}
 		last: ${webhook.at(-1)}`);
-	}
-	await fetch(webhook, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ text: message }),
-	});
+  }
+  await fetch(webhook, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: message }),
+  });
 }
 
 for (const _ of new Array(3).fill(0)) {
-	try {
-		await main();
-		break;
-	} catch (err) {
-		console.error(err);
-	}
+  try {
+    await main();
+    break;
+  } catch (err) {
+    console.error(err);
+  }
 }
--- validator.ts
+++ validator.ts
@@ -8,71 +8,71 @@ https://developers.notion.com/docs/working-with-databases
 // S: Schema
 // never throws error, purely for logging purpose.
 export function check<S extends v.BaseSchema<unknown, unknown, v.BaseIssue<unknown>>>(
-	varName: string,
-	val: unknown,
-	schema: S,
+  varName: string,
+  val: unknown,
+  schema: S,
 ): {
-	val: v.InferOutput<S>;
-	err?: string;
+  val: v.InferOutput<S>;
+  err?: string;
 } {
-	type O = v.InferOutput<S>; // output
+  type O = v.InferOutput<S>; // output
 
-	const result = v.safeParse(schema, val);
-	if (result.success) return { val: result.output satisfies O };
+  const result = v.safeParse(schema, val);
+  if (result.success) return { val: result.output satisfies O };
 
-	const error = result.issues.join(", ");
-	console.error(`WARNING: Failed to parse schema of ${varName}: ${error}`);
-	return {
-		err: error,
-		val: val as O, // just let it go until it crashes for `cannot access property of undefined` or worse, sends undefined to slack	};
-	};
+  const error = result.issues.join(", ");
+  console.error(`WARNING: Failed to parse schema of ${varName}: ${error}`);
+  return {
+    err: error,
+    val: val as O, // just let it go until it crashes for `cannot access property of undefined` or worse, sends undefined to slack	};
+  };
 }
 
 export class TypeChecker {
-	check<S extends v.BaseSchema<unknown, unknown, v.BaseIssue<unknown>>>(name: string, val: unknown, schema: S) {
-		const { val: parsed, err } = check(name, val, schema);
-		if (err) {
-			this.failedCount++;
-			this.errors += `\n- ${err}`;
-		}
-		return parsed;
-	}
-	failedCount = 0;
-	errors = "";
-	hasFailed() {
-		return this.failedCount > 0;
-	}
+  check<S extends v.BaseSchema<unknown, unknown, v.BaseIssue<unknown>>>(name: string, val: unknown, schema: S) {
+    const { val: parsed, err } = check(name, val, schema);
+    if (err) {
+      this.failedCount++;
+      this.errors += `\n- ${err}`;
+    }
+    return parsed;
+  }
+  failedCount = 0;
+  errors = "";
+  hasFailed() {
+    return this.failedCount > 0;
+  }
 }
 
 export const Url = v.pipe(v.string(), v.url());
 
 const NotionDate = v.object({
-	type: v.literal("date"),
-	date: v.object({
-		start: v.pipe(v.string(), v.regex(/\d{4}-\d{2}-\d{2}/) /* yyyy-MM-dd */),
-	}),
+  type: v.literal("date"),
+  date: v.object({
+    start: v.pipe(v.string(), v.regex(/\d{4}-\d{2}-\d{2}/) /* yyyy-MM-dd */),
+  }),
 });
 const NotionText = v.object({
-	type: v.literal("text"),
-	text: v.object({
-		content: v.string(),
-	}),
-	plain_text: v.string(),
+  type: v.literal("text"),
+  text: v.object({
+    content: v.string(),
+  }),
+  plain_text: v.string(),
 });
 const NotionTitle = v.object({
-	type: v.literal("title"),
-	title: v.array(v.union([NotionText])),
+  type: v.literal("title"),
+  title: v.array(v.union([NotionText])),
 });
 
 // API REFERENCE[user]: https://developers.notion.com/reference/user
 const NotionUser = v.object({
-	type: v.union([v.literal("person"), v.literal("bot")]),
-	name: v.string(),
+  type: v.union([v.literal("person"), v.literal("bot")]),
+  name: v.string(),
 }); // TODO
 
 export const NotionTypes = {
-	date: NotionDate,
-	text: NotionText,
-	title: NotionTitle,
-	user: NotionUser,
+  date: NotionDate,
+  text: NotionText,
+  title: NotionTitle,
+  user: NotionUser,
 };
